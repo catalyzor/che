38a35f655ca0d3d431775cba0427b32913f5912b
Ann Shumilova <ashumilova@codenvy.com>
2016-11-25 15:45:31 +0200

    CODENVY-802: add filter selector widget
    Signed-off-by: Ann Shumilova <ashumilova@codenvy.com>

--- dashboard/src/components/widget/list/che-list-header.directive.ts
+++ dashboard/src/components/widget/list/che-list-header.directive.ts
@@ -35,9 +35,11 @@ export class CheListHeader {
       onAdd: '&?cheOnAdd',
       deleteButtonTitle: '@?cheDeleteButtonTitle',
       onDelete: '&?cheOnDelete',
+      filterValues: '=?cheFilterValues',
       hideDelete: '=?cheHideDelete',
       hideSearch: '=?cheHideSearch',
-      hideHeader:'=?cheHideHeader'
+      hideHeader:'=?cheHideHeader',
+      hideFilter: '=?cheHideFilter'
     };
   }
 }
